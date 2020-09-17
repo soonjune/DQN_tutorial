ed39ad2518be1d24b730fd732a5d22b654f0a3ee
soonjune <tino7060@likelion.org>
2020-09-17 21:29:47 +0900

    policy

--- test.py
+++ test.py
@@ -2,6 +2,23 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+
+# MAP
+"4x4": [
+    "SFFF",
+    "FHFH",
+    "FFFH",
+    "HFFG"
+]
+
+"""
+policy = {0: 1, 1: 2, 2: 1, 3: 0, 4: 1, 6: 1, 8: 2, 9: 1, 10: 1, 13: 2, 14: 2}
+
 env = gym.make('FrozenLake-v0')
 
 n_games = 1000
@@ -12,7 +29,7 @@ for i in range(n_games):
     obs = env.reset()
     score = 0
     while not done:
-        action = env.action_space.sample()
+        action = policy[obs]
         obs, reward, done, info = env.step(action)
         score += reward
     scores.append(score)
